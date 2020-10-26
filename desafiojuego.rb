ea96ff713b31c582fedb6a7e52be5411d92122c6
Gilibeth Vega <gvega@creditu.com>
2020-10-26 19:57:01 -0300

    se completa desafío con todos los parametros exigidos

--- desafiojuego.rb
+++ desafiojuego.rb
@@ -17,28 +17,58 @@ def juego_cachipun(opcion_jugador1, opcion_jugador2)
 end
 
 puts "Hola Jugador 1 escoge el número de la opción que deseas jugar"
-imprimir_menu
-opcion_jugador1 = gets.chomp.to_s
+opcion_jugador1 = 'cualquier valor'
+while opcion_jugador1 != 'salir' && opcion_jugador1 != 'Salir'  && opcion_jugador1 != '1'  && opcion_jugador1 != '2'  && opcion_jugador1 != '3'
+  imprimir_menu
+  opcion_jugador1 = gets.chomp.to_s
   if opcion_jugador1 == "1"
     puts "Hola Jugador 2 escoge el número de la opción que deseas jugar"
-    imprimir_menu
-    opcion_jugador2 = gets.chomp.to_s
-    juego_cachipun(opcion_jugador1, opcion_jugador2)
+    opcion_jugador2 = "cualquier valor"
+    while opcion_jugador2 != 'salir' && opcion_jugador2 != 'Salir'  && opcion_jugador2 != '1'  && opcion_jugador2 != '2'  && opcion_jugador2 != '3'
+      imprimir_menu
+      opcion_jugador2 = gets.chomp.to_s
+      if opcion_jugador2 == "Salir" || opcion_jugador2 == "salir" || opcion_jugador2 == "4"
+        puts "Saliendo"
+      elsif opcion_jugador2 == "1" || opcion_jugador2 == "2" || opcion_jugador2 == "3"
+        juego_cachipun(opcion_jugador1, opcion_jugador2)
+      else
+        puts "Opción inválida ingrese: 1, 2, 3 o salir" 
+      end
+    end
   elsif opcion_jugador1 == "2"
     puts "Hola Jugador 2 escoge el número de la opción que deseas jugar"
-    imprimir_menu
-    opcion_jugador2 = gets.chomp.to_s
-    juego_cachipun(opcion_jugador1, opcion_jugador2)
+    opcion_jugador2 = "cualquier valor"
+    while opcion_jugador2 != 'salir' && opcion_jugador2 != 'Salir'  && opcion_jugador2 != '1'  && opcion_jugador2 != '2'  && opcion_jugador2 != '3'
+      imprimir_menu
+      opcion_jugador2 = gets.chomp.to_s
+      if opcion_jugador2 == "Salir" || opcion_jugador2 == "salir" || opcion_jugador2 == "4"
+        puts "Saliendo"
+      elsif opcion_jugador2 == "1" || opcion_jugador2 == "2" || opcion_jugador2 == "3"
+        juego_cachipun(opcion_jugador1, opcion_jugador2)
+      else
+        puts "Opción inválida ingrese: 1, 2, 3 o salir"
+      end
+    end
   elsif opcion_jugador1 == "3"
     puts "Hola Jugador 2 escoge el número de la opción que deseas jugar"
-    imprimir_menu
-    opcion_jugador2 = gets.chomp.to_s
-    juego_cachipun(opcion_jugador1, opcion_jugador2)
+    opcion_jugador2 = "cualquier valor"
+    while opcion_jugador2 != 'salir' && opcion_jugador2 != 'Salir'  && opcion_jugador2 != '1'  && opcion_jugador2 != '2'  && opcion_jugador2 != '3'
+      imprimir_menu
+      opcion_jugador2 = gets.chomp.to_s
+      if opcion_jugador2 == "Salir" || opcion_jugador2 == "salir" || opcion_jugador2 == "4"
+        puts "Saliendo"
+      elsif opcion_jugador2 == "1" || opcion_jugador2 == "2" || opcion_jugador2 == "3"
+        juego_cachipun(opcion_jugador1, opcion_jugador2)
+      else
+        puts "Opción inválida ingrese: 1, 2, 3 o salir" 
+      end
+    end
   elsif  opcion_jugador1 == "Salir" || opcion_jugador1 == "salir" || opcion_jugador1 == "4"
     puts "Saliendo"
   else  
     puts "Opción inválida ingrese: 1, 2, 3 o salir"
   end
+end
 
 
 
