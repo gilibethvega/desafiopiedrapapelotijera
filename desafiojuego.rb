e4be1f0fc5c6c2c0d04cdbd635e1020a72415472
Gilibeth Vega <gvega@creditu.com>
2020-10-26 18:36:05 -0300

    se incluye logica de desafío

--- desafiojuego.rb
+++ desafiojuego.rb
@@ -1,26 +1,49 @@
-juego_users = ARGV[0]
-juego_pc = rand(0-3)
-
-jugada = 0 if juego_users == 'piedra'
-jugada = 1 if juego_users == 'papel'
-jugada = 2 if juego_users == 'tijera'
-
-jugada_pc = 'piedra' if juego_pc == 0
-jugada_pc= 'papel' if juego_pc == 1
-jugada_pc = 'tijera' if juego_pc == 2
-
-if jugada != 0 && jugada != 1 && jugada != 2
-  puts "Argumento inválido: debes ingresar piedra, papel o tijera"
-elsif (juego_pc == 0 && jugada == 0) || (juego_pc == 1 && jugada == 1) || (juego_pc == 2 && jugada == 2)
-  puts "Empataste"
-  puts "El computador jugo #{jugada_pc}"
-elsif (juego_pc == 0 && jugada == 2) || (juego_pc == 1 && jugada == 0) || (juego_pc == 2 && jugada == 1)
-  puts "Perdiste"
-  puts "El computador jugo #{jugada_pc}" 
-else (juego_pc == 2 && jugada == 0) || (juego_pc == 0 && jugada == 1) || (juego_pc == 1 && jugada == 2)
-  puts "Ganaste"
-  puts "El computador jugo #{jugada_pc}"
+def imprimir_menu
+  puts "Menú"
+  puts "1 - Piedra"
+  puts "2 - Papel"
+  puts "3 - Tijera"
+  puts "4 - Salir"
 end
+
+def juego_cachipun(opcion_jugador1, opcion_jugador2)
+  if opcion_jugador1 == opcion_jugador2
+    puts "Ha sido un empate"
+  elsif (opcion_jugador1 == "1"  && opcion_jugador2 == "2") || (opcion_jugador1 == "2"  && opcion_jugador2 == "3") || (opcion_jugador1 == "3"  && opcion_jugador2 == "1")
+    puts "El jugador 2 ha ganado la partida"
+  elsif (opcion_jugador1 == "1"  && opcion_jugador2 == "3") || (opcion_jugador1 == "2"  && opcion_jugador2 == "1") || (opcion_jugador1 == "3"  && opcion_jugador2 == "2")
+    puts "El jugador 1 ha ganado la partida"
+  end
+end
+
+puts "Hola Jugador 1 escoge el número de la opción que deseas jugar"
+imprimir_menu
+opcion_jugador1 = gets.chomp.to_s
+  if opcion_jugador1 == "1"
+    puts "Hola Jugador 2 escoge el número de la opción que deseas jugar"
+    imprimir_menu
+    opcion_jugador2 = gets.chomp.to_s
+    juego_cachipun(opcion_jugador1, opcion_jugador2)
+  elsif opcion_jugador1 == "2"
+    puts "Hola Jugador 2 escoge el número de la opción que deseas jugar"
+    imprimir_menu
+    opcion_jugador2 = gets.chomp.to_s
+    juego_cachipun(opcion_jugador1, opcion_jugador2)
+  elsif opcion_jugador1 == "3"
+    puts "Hola Jugador 2 escoge el número de la opción que deseas jugar"
+    imprimir_menu
+    opcion_jugador2 = gets.chomp.to_s
+    juego_cachipun(opcion_jugador1, opcion_jugador2)
+  elsif  opcion_jugador1 == "Salir" || opcion_jugador1 == "salir" || opcion_jugador1 == "4"
+    puts "Saliendo"
+  else  
+    puts "Opción inválida ingrese: 1, 2, 3 o salir"
+  end
+
+
+
+
+
 
 
 
